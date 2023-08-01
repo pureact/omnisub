9f0541bc74080954a22d547c8093e287bec51c0d
naek <naek2k@outlook.com>
2023-08-01 10:38:41 -0400

    fixing setup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 """Setup for omnisub."""
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name="omnisub",
     version="1.0",
     description="omnisub",
     author="naek",
-    packages=["omnisub"],
+    packages=find_packages(),
     entry_points={"console_scripts": ["omnisub=omnisub.__main__:main"]},
 )
